e384ff291007caa67618ac7d34e233d7f2e18667
Mahesh <maheshpdahake@gmail.com>
2025-01-13 19:23:10 +0530

    Update vercelJSON file

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button } from '../button';
 
 function Header() {
+
+  const user = JSON.parse(localStorage.getItem('users'));
+
+  useEffect(() => {
+    console.log(user)
+  }, [])
   return (
     <div className='p-3 shadow-sm flex justify-between items-center px-5'>
-        <div className='flex items-center space-x-3'>
-        <img src='/logo.svg'/>
+      <div className='flex items-center space-x-3'>
+        <img src='/logo.svg' />
         <h1 className='font-bold items-start'>Fixizl</h1>
-        </div> 
+      </div>
       <div>
-        <Button>Sign In</Button>
+        {user ?
+          <div>
+
+          </div>
+          :
+          <Button>Sign In</Button>}
       </div>
     </div>
   );
